0ebf29bf55643ad09fd504fa74f4f363e72c8fa7
belaymit <2belamit@gmail.com>
2023-02-01 14:32:47 +0300

    removed fetching data from home to App component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,32 +1,14 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import MovieList from '../movieList/MovieList';
-import axiosApi from '../../common/apis/axiosApi';
-import { ApiKey } from '../../common/apis/ApiKey';
-import { addMovies } from '../../redux/movieSlice';
+
 import '../../styles/home.scss';
 
-const Home = () => {
-  const text = 'black';
-  const dispatch = useDispatch();
-  const fetchMovie = async () => {
-    const response = await axiosApi.get(
-      `?apiKey=${ApiKey}&s=${text}&type=movie`,
-    );
-    dispatch(addMovies(response.data));
-  };
-
-  useEffect(() => {
-    fetchMovie();
-  }, []);
-
-  return (
-    <>
-      <div className="movie-img">
-        <MovieList />
-      </div>
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <div className="movie-img">
+      <MovieList />
+    </div>
+  </>
+);
 
 export default Home;
