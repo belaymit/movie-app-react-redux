83c02c85bb94893adb1214decd796030df4431af
belaymit <2belamit@gmail.com>
2023-02-01 14:33:36 +0300

    added shows to Shows component

--- src/components/shows/Shows.jsx
+++ src/components/shows/Shows.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getAllShows } from '../../redux/movieSlice';
+import Card from '../card/Card';
+import '../../styles/movieList.scss';
 
-const Shows = () => (
-  <div>Shows</div>
-);
+const Shows = () => {
+  const shows = useSelector(getAllShows);
+  let renderMovies = '';
+
+  renderMovies = shows.Response === 'True' ? (
+    shows.Search.map((show) => (
+      <Card key={show.index} data={show} />
+    ))
+  ) : (
+    <div className="movies-error"><h3>{shows.error}</h3></div>
+  );
+
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h3>Shows</h3>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Shows;
